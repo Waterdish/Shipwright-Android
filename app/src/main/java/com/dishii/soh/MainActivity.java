16176052dc4d0ad2c7cc17e7aeb5220f652735ec
Waterdish <waterdish101@gmail.com>
2023-10-15 12:13:43 -0700

    First commit to main

--- app/src/main/java/com/dishii/soh/MainActivity.java
+++ app/src/main/java/com/dishii/soh/MainActivity.java
@@ -17,47 +17,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import android.util.Log;
 
-//This class is the main SDLActivity and just sets up a bunch of default files
+//This class is the main SDLActivity
 public class MainActivity extends SDLActivity{
-
-    private static final int STORAGE_PERMISSION_REQUEST_CODE = 1;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Check if storage permissions are granted
-        if (Environment.isExternalStorageManager()) {
-            //setupFiles();
-        } else {
-            requestStoragePermission();
-        }
-    }
-
-        // Request storage permission
-        private void requestStoragePermission() {
-            try {
-                Uri uri = Uri.parse("package:" + BuildConfig.APPLICATION_ID);
-                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, uri);
-                startActivity(intent);
-            } catch (Exception ex){
-                Intent intent = new Intent();
-                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
-                startActivity(intent);
-            }
-        }
-
-        // Handle permission request result
-        @Override
-        public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-            if (requestCode == STORAGE_PERMISSION_REQUEST_CODE) {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //setupFiles();
-                } else {
-                    // Permission denied, handle accordingly (e.g., show a message)
-                }
-            }
-        }
 
 }
